75860022f7df0637a398c8dc9c3431b540ec5a32
yuichiyasui <yassan.b.a.w@gmail.com>
2023-08-04 00:44:29 +0900

    chore: Add eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,10 +7,20 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:storybook/recommended",
     "prettier",
   ],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
